728245877dc4d483d4810a8d28e989a5b1312a5e
camsiabor <kkidthelordzlight@163.com>
2019-07-15 01:18:33 +0800

    lua integration

--- luar/extra.go
+++ luar/extra.go
@@ -88,3 +88,88 @@ func SetLuaPath(L *lua.State, luaPath, luaCPath string) (err error) {
 
 	return nil
 }
+
+func GetVal(L *lua.State, idx int) (interface{}, error) {
+	if L.IsNoneOrNil(idx) {
+		return nil, nil
+	}
+	var ltype = int(L.Type(idx))
+	switch ltype {
+	case int(lua.LUA_TNUMBER):
+		return L.ToNumber(idx), nil
+	case int(lua.LUA_TSTRING):
+		return L.ToString(idx), nil
+	case int(lua.LUA_TBOOLEAN):
+		return L.ToBoolean(idx), nil
+	}
+	var r interface{}
+	var err = luar.LuaToGo(L, idx, &r)
+	return r, err
+}
+
+func FormatStack(stacks []lua.LuaStackEntry) []lua.LuaStackEntry {
+	var count = len(stacks)
+	var clones = make([]lua.LuaStackEntry, count)
+	for i := 0; i < count; i++ {
+		var stack = stacks[i]
+		var clone = lua.LuaStackEntry{
+			Name: stack.Name,
+		}
+		var linenum = stack.CurrentLine
+		if linenum >= 0 {
+			var lines = strings.Split(stack.Source, "\n")
+			if linenum < len(lines) {
+				clone.ShortSource = lines[linenum-1]
+			} else {
+				clone.ShortSource = stack.ShortSource
+			}
+		}
+		clone.Source = ""
+		clone.CurrentLine = linenum
+		clones[i] = clone
+	}
+	return clones
+}
+
+func FormatStackToString(stacks []lua.LuaStackEntry, prefix string, suffix string) string {
+	var str = ""
+	var count = len(stacks)
+
+	for i := 0; i < count; i++ {
+		var stack = stacks[i]
+		var source = stack.ShortSource
+		var funcname = stack.Name
+		var linenum = stack.CurrentLine
+		if linenum >= 0 {
+			var lines = strings.Split(stack.Source, "\n")
+			if linenum < len(lines) {
+				source = lines[linenum-1]
+			}
+		}
+		var one = fmt.Sprintf("%s%s %s %d\n%s", prefix, source, funcname, linenum, suffix)
+		str = str + one
+	}
+	return str
+}
+
+func FormatStackToMap(stacks []lua.LuaStackEntry) []map[string]interface{} {
+	var count = len(stacks)
+	var clones = make([]map[string]interface{}, count)
+	for i := 0; i < count; i++ {
+		var stack = stacks[i]
+		var clone = make(map[string]interface{})
+		var linenum = stack.CurrentLine
+		if linenum >= 0 {
+			var lines = strings.Split(stack.Source, "\n")
+			if linenum < len(lines) {
+				clone["linesrc"] = lines[linenum-1]
+			} else {
+				clone["linesrc"] = stack.ShortSource
+			}
+		}
+		clone["line"] = linenum
+		clone["func"] = stack.Name
+		clones[i] = clone
+	}
+	return clones
+}
